6f3b8e1ec895b4a880243f49714151dd4fc22b5d
NingqianJia <ricky.jia2018@gmail.com>
2023-07-05 00:20:46 -0700

    update

---
+++ Array/704. BinarySearch/main.go
@@ -0,0 +1,36 @@
+/*
+Leetcode 704
+*/
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func BinarySearch(nums []int, target int) int {
+
+	left := 0
+	right := len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid
+		} else {
+			return mid
+		}
+	}
+
+	return -1
+}
+func main() {
+	var arr []int = []int{1, 2, 3, 4, 5, 6}
+	sort.Ints(arr)
+	fmt.Println(arr)
+	target := 4
+	index := BinarySearch(arr, target)
+	fmt.Println(index)
+}
