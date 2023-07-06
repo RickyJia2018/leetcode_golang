54baea2e55b6121b071590467b6521064021911c
NingqianJia <ricky.jia2018@gmail.com>
2023-07-05 19:45:18 -0700

    update the second way of Binarysearch

--- Array/704. BinarySearch/main.go
+++ Array/704. BinarySearch/main.go
@@ -26,6 +26,24 @@ func BinarySearch(nums []int, target int) int {
 
 	return -1
 }
+
+func BinarySearch2(nums []int, target int) int {
+
+	left, mid := 0, 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid = left + (right-left)/2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
 func main() {
 	var arr []int = []int{1, 2, 3, 4, 5, 6}
 	sort.Ints(arr)
