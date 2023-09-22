5a78fe449ef0f402809191b437b2cb93c963acd6
ricky <ricky.jia@funglyn.ca>
2023-09-22 14:25:00 -0400

    update

---
+++ 142.linked-list-cycle-ii.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=142 lang=golang
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+    slow := head
+	fast := head
+
+	for fast!= nil && fast.Next!= nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+          //Found a cycle, start from meet point until 
+		  //meet again. new meet point is the entry point 
+		  //of the cycle.
+		  slow = head
+		  for slow != fast{
+			slow=slow.Next
+			fast=fast.Next
+		  }
+		  return slow
+        }
+	}
+	return nil
+}
+// @lc code=end
+
