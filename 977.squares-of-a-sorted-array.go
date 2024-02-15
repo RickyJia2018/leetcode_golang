cb37f19f16d7e7475ddb7bbcd57a2fc3e33cf99f
ricky.jia <ricky.jia2018@gmail.com>
2024-02-15 15:10:13 -0500

    update

--- 977.squares-of-a-sorted-array.go
+++ 977.squares-of-a-sorted-array.go
@@ -6,7 +6,28 @@
 
 // @lc code=start
 func sortedSquares(nums []int) []int {
-    
+    left:=0
+	right:=len(nums)-1
+	index := right
+	res := make([]int,len(nums))  // need a temp array to store result
+
+	for index>=0{  //don't forget index 0
+	
+		leftNum :=nums[left]*nums[left]
+		rightNum :=nums[right]*nums[right]
+
+		if leftNum<=rightNum{
+			res[index] = rightNum
+			right--
+		}else {
+			res[index] = leftNum
+			left++
+		}
+		index --
+		fmt.Println(index)
+		
+	}
+	return res
 }
 // @lc code=end
 
