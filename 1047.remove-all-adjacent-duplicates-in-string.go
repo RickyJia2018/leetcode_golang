aac2f6030638bae8b69b3e6f425240ec0b68301a
ricky.jia <ricky.jia2018@gmail.com>
2024-02-22 14:22:28 -0500

    update

--- 1047.remove-all-adjacent-duplicates-in-string.go
+++ 1047.remove-all-adjacent-duplicates-in-string.go
@@ -7,18 +7,25 @@
 // @lc code=start
 func removeDuplicates(s string) string {
 
-    stack := make([]byte, 0)
-	for i:=0;i<len(s);i++{
-		if len(stack)>0 && stack[len(stack)-1]==s[i] {
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		if len(stack) == 0 || stack[len(stack)-1] != s[i] {
+			stack = append(stack, s[i])
+		} else {
 			stack = stack[:len(stack)-1]
-		}else{
-			stack = append(stack,s[i])
 		}
 	}
 	return string(stack)
+	// stack := make([]byte, 0)
+	// for i := 0; i < len(s); i++ {
+	// 	if len(stack) > 0 && stack[len(stack)-1] == s[i] {
+	// 		stack = stack[:len(stack)-1]
+	// 	} else {
+	// 		stack = append(stack, s[i])
+	// 	}
+	// }
+	// return string(stack)
 }
-
-
 
 // @lc code=end
 
--- 20.valid-parentheses.go
+++ 20.valid-parentheses.go
@@ -6,52 +6,77 @@
 
 // @lc code=start
 func isValid(s string) bool {
-    if len(s) == 0 {
-		return true
+	if len(s) <= 1 {
+		return false
 	}
-	hashMap := map[byte]byte{
+	m := map[byte]byte{
 		'(': ')',
 		'{': '}',
-        '[': ']',
+		'[': ']',
 	}
-	stack := make([]byte,0)
+	myStack := make([]byte, 0)
+	b := []byte(s)
+	for i := 0; i < len(b); i++ {
+		if c, has := m[b[i]]; has { // if is left side  push to stack
+			myStack = append(myStack, c)
+		} else { //right side, compare with top item in stack
 
-
-	for i := 0; i < len(s); i++ {
-		if s[i] == '(' || s[i] == '{' || s[i] =='['{
-			stack = append(stack, hashMap[s[i]])
-		}else{
-			if len(stack) == 0 {
+			if len(myStack) <= 0 || b[i] != myStack[len(myStack)-1] {
 				return false
 			}
-			if s[i] == stack[len(stack)-1] {
-				stack = stack[:len(stack)-1]
-			}else {
-				return false
-			}
+			myStack = myStack[:len(myStack)-1] //pop from stack
 		}
-	
-    }
-	return len(stack) == 0
+	}
+	return len(myStack) == 0
 
 }
-func isValid2(s string) bool {
-    hash := map[byte]byte{')':'(', ']':'[', '}':'{'}
-    stack := make([]byte, 0)
-    if s == "" {
-        return true
-    }
 
-    for i := 0; i < len(s); i++ {
-        if s[i] == '(' || s[i] == '[' || s[i] == '{' {
-            stack = append(stack, s[i])
-        } else if len(stack) > 0 && stack[len(stack)-1] == hash[s[i]] {
-            stack = stack[:len(stack)-1]
-        } else {
-            return false
-        }
-    }
-    return len(stack) == 0
-}
+// func isValid(s string) bool {
+//     if len(s) == 0 {
+// 		return true
+// 	}
+// 	hashMap := map[byte]byte{
+// 		'(': ')',
+// 		'{': '}',
+//         '[': ']',
+// 	}
+// 	stack := make([]byte,0)
+
+// 	for i := 0; i < len(s); i++ {
+// 		if s[i] == '(' || s[i] == '{' || s[i] =='['{
+// 			stack = append(stack, hashMap[s[i]])
+// 		}else{
+// 			if len(stack) == 0 {
+// 				return false
+// 			}
+// 			if s[i] == stack[len(stack)-1] {
+// 				stack = stack[:len(stack)-1]
+// 			}else {
+// 				return false
+// 			}
+// 		}
+
+//     }
+// 	return len(stack) == 0
+
+// }
+// func isValid2(s string) bool {
+//     hash := map[byte]byte{')':'(', ']':'[', '}':'{'}
+//     stack := make([]byte, 0)
+//     if s == "" {
+//         return true
+//     }
+
+//     for i := 0; i < len(s); i++ {
+//         if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+//             stack = append(stack, s[i])
+//         } else if len(stack) > 0 && stack[len(stack)-1] == hash[s[i]] {
+//             stack = stack[:len(stack)-1]
+//         } else {
+//             return false
+//         }
+//     }
+//     return len(stack) == 0
+// }
 // @lc code=end
 
